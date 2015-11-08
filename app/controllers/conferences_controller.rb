e76215bd6b9fc05142a93e45f0b917d9bbc543f3
Andrew Peterson <AndrewPetersonSoftworks@gmail.com>
2015-11-08 03:34:36 -0500

    Made hotels only show on map to prove that the markers could be added

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -18,6 +18,9 @@ class ConferencesController < ApplicationController
     @client = ::GooglePlaces::Client.new("AIzaSyA0-ah-qzAdf7c6IvSSQNlfY4LTNUuZq1U")
     @restaurants = @client.spots(@conference.latitude, @conference.longitude, :types => ['restaurant', 'food'])
     @hotels = @client.spots(@conference.latitude, @conference.longitude, :types => 'hotel')
+    @hotels.each |hotel| do
+      hotel[:infowindow] = hotel.name
+    end
     @transportation = @client.spots(@conference.latitude, @conference.longitude, :types => ['subway_station', 'bus_station', 'car_rental', 'train_station', 'taxi_stand', 'travel_agency'])
     @funstuff = @client.spots(@conference.latitude, @conference.longitude, :types => ['amusement_park', 'book_store', 'bar', 'museum', 'casino', 'dentist'])
   end
